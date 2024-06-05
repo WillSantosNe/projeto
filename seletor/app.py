60311e7d934490eff740ba0effbacdc69210fd1f
Mateus Stanislauki Bassi <132794039+Mateus-Bassi@users.noreply.github.com>
2024-06-05 19:41:12 -0300

    Adicionando classe validador
    Validador adicionado

--- seletor/app.py
+++ seletor/app.py
@@ -1,1 +1,27 @@
+from time import time
+from flask import Flask, request, redirect, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dataclasses import dataclass
+from datetime import date, datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#Criando objeto validador
+@dataclass
+class Validador(db.Model):
+    id: int
+    nome: str
+    flag:int
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(20), unique=False, nullable=False)
+    flag = db.Column(db.Integer)
+
+#rotas do validador 
 print("testando docker")
+
